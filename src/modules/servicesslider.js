a376d8e8b3c6658de9449cbdff061cce9205a6d3
SantYaga33 <45719691+SantYaga33@users.noreply.github.com>
2019-08-15 14:26:33 +0400

    + слайд сервисы

--- src/modules/servicesslider.js
+++ src/modules/servicesslider.js
@@ -1,52 +1,27 @@
-// import animateCSS from "./animateCss";
 
 const servicesSlider = (minWidth = 223) => {
   const slide = document.querySelectorAll('.services-slider .slide'),
     slider = document.querySelector('.services-slider'),
     sliderWrapper = document.querySelector('#services .wrapper'),
     maxWidth = minWidth * (slide.length - 5);
-  let prevArr,
-    nextArr,
-    currentWidth = 0,
-    h2 = document.querySelector('#services .wrapper h2'),
-    html = `
-                <div class="slider-arrow next"><span><i class="fa fa-angle-right"></i></span></div>
-                <div class="slider-arrow prev"><span><i class="fa fa-angle-left"></i></span></div>
-            `;
-  slide.forEach((elem, index) => {
-    if (index === 0) {
-      elem.style.cssText = `
-                -webkit-transition: .2s linear;
-                -moz-transition: .2s linear;
-                -o-transition: .2s linear;
-                transition: .2s linear;
-                min-width: 210px;
-            `;
-    } else {
-      elem.style.cssText = `min-width: 210px;`;
-    }
 
-  });
-  //корректируем обертку
+  let currentWidth = 0,
+    h2 = document.querySelector('#services .wrapper h2'),
+    allArrrElem = `
+            <div class="slider-arrow next"><span><img src="images/arrow-right.png"></span></div>
+            <div class="slider-arrow prev"><span><img src="images/arrow-left.png"></span></div>
+            `;
+
+  //корректируем верстку
+  slide[0].style.marginLeft = 0;
   sliderWrapper.style.cssText = `max-width: 1150px`;
-  //освобождаем  wrapper
   document.getElementById('services').insertAdjacentHTML('afterbegin', h2.outerHTML);
   h2.remove();
-  //добавляем стрелки стили и анимацию
-  slider.style.cssText = `
-        position: relative;
-        overflow-x: hidden;
-    `;
-  slider.insertAdjacentHTML('beforeEnd', html);
-  nextArr = slider.querySelector('.slider-arrow.next');
-  prevArr = slider.querySelector('.slider-arrow.prev');
+  slider.insertAdjacentHTML('beforeEnd', allArrrElem);
 
   const moveSlide = (changeSlide) => {
     currentWidth += +changeSlide;
-
-
     if (currentWidth <= 6) {
-      console.log(currentWidth);
       currentWidth = 4;
       slide[0].style.marginLeft = `-${currentWidth}px`;
       currentWidth = 0;
